2f802c3181c96c09feeafc225b0693065890eaca
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2025-08-27 17:09:52 -0300

    Aprendendo Data Binding

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -5,53 +5,53 @@ sap.ui.define([
 
     return Controller.extend("zov.controller.View1", {
         onInit: function () {
-            // model padrão da view
-            /* var oView = this.getView();
-            var oModel = new sap.ui.model.json.JSONModel();
-            oModel.setData({ "usuario": { "nome": "Marcus Vinícius" } });
-            oView.setModel(oModel); */
-
-            //model com o nome "dados"
             var oView = this.getView();
-            var oModel = new sap.ui.model.json.JSONModel();
-            oModel.setData({ "usuario": { "nome": "João Victor" } });
-            oView.setModel(oModel, "dados");
+            
+            var oModel1 = new sap.ui.model.json.JSONModel();
+            oModel1.setData({ "usuario": { "nome": "José" } });
+            oModel1.setDefaultBindingMode(sap.ui.model.BindingMode.OneWay);
+            oView.setModel(oModel1, "oneway");
+
+            var oModel2 = new sap.ui.model.json.JSONModel();
+            oModel2.setData({ "usuario": { "nome": "José" } });
+            oModel2.setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
+            oView.setModel(oModel2, "twoway");
+
+            var oModel3 = new sap.ui.model.json.JSONModel();
+            oModel3.setData({ "usuario": { "nome": "José" } });
+            oModel3.setDefaultBindingMode(sap.ui.model.BindingMode.OneTime);
+            oView.setModel(oModel3, "onetime");
         },
 
-        onTestModels: function (oEvent) {
-            //model i18n
-            var o18n = this.getView().getModel("i18n").getResourceBundle();
-            var sText = o18n.getText("title");
+		onTestOneWay: function(oEvent) {
+			var oView = this.getView();
 
-            console.log("Texto com a chave 'title'");
-            console.log(sText);
-
-            console.log("----------------------------------------");
-
-            //model de usuários
-            var oModel = this.getOwnerComponent().getModel("usuarios");
+            var oModel = oView.getModel("oneway");
             var oData = oModel.getData();
-            console.log("Model dos usuários");
-            console.log(oData);
+            oData.usuario.nome += ".";
+            oModel.setData(oData);
+            //oView.setModel(oModel);
+		},
 
-            console.log("----------------------------------------");
+		onTestTwoWay: function(oEvent) {
+			var oView = this.getView();
 
-            //model do serviço
-            var oModel = this.getOwnerComponent().getModel();
-            oModel.read("/OVCabSet", {
-                success: function (oData, oResponse) {
-                    console.log("Dados retornados do serviço");
-                    console.log(oData);
-                    console.log(oResponse);
-                },
-                error: function (oError) {
-                    console.log(oError);
-                }
-            })
+            var oModel = oView.getModel("twoway");
+            var oData = oModel.getData();
+            oData.usuario.nome += ".";
+            oModel.setData(oData);
+            //oView.setModel(oModel);
+		},
 
-        }
+		onTestOneTime: function(oEvent) {
+			var oView = this.getView();
 
-
+            var oModel = oView.getModel("onetime");
+            var oData = oModel.getData();
+            oData.usuario.nome += ".";
+            oModel.setData(oData);
+            //oView.setModel(oModel);
+		}
 
     });
 });
