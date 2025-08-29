453cebd2b4ab764bff3f85ba27f0377c5b454f55
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2025-08-29 15:40:45 -0300

    Aprendendo Dialog

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,9 +9,30 @@ sap.ui.define([
         onInit: function() {
         },
 
-		onHeader: function(oEvent) {
-			alert("onHeader");
-		}
+		onAbrirDialogInfo: function(oEvent) {
+			var that = this;
+            var sName = "zov.view.DialogInfo";
+
+            if(!this.oDialog){
+                // this.loadFragment({
+                //     name: sName
+                // }).then(function(oDialog2) {
+                //     that.oDialog = oDialog2;
+                //     that.oDialog.open();
+                // }.bind(this));
+
+                this.oDialog = this.byId("DialogInfo");
+                that.oDialog.open();
+
+            } else {
+                this.oDialog.open();
+            }
+		},
+
+		onFecharDialogInfo: function(oEvent) {
+			// this.byId("DialogInfo").close();
+            this.oDialog.close();
+        }
 
     });
 });
